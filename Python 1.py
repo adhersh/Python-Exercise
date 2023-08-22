0f63060f86d45bd271297f15b54188e69264ad78
adhersh <unniadhersh@gmail.com>
2023-08-22 11:10:32 +0530

    modified:   Python 1.py

---
+++ Python 1.py
@@ -0,0 +1,8 @@
+def add(a,b):
+    return a + b
+
+num1 = float(input("enter 1 st number"))
+num2 = float(input("enter 2nd number"))
+
+sum= add(num1,num2)
+print(f" the sum of {num1} and {num2} are {sum}")
